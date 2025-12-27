998675b1abb31a146c963e3bb0b3aeb7f95c235e
Andika Zulprisa Adha <andikazulprisa27@gmail.com>
2025-12-27 16:22:41 +0700

    add preloader

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,11 @@ import App from "./App.jsx";
 import Navbar from "./components/Navbar.jsx";
 import "remixicon/fonts/remixicon.css";
 import Footer from "./components/Footer.jsx";
+import Preloader from "./components/Preloader.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
+    <Preloader />
     <div className="container mx-auto px-4">
       <Navbar />
       <App />
