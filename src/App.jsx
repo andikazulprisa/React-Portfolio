76318884853fab42c651bf57ceed18a7768db157
Andika Zulprisa Adha <andikazulprisa27@gmail.com>
2025-12-27 17:50:15 +0700

    feat: add animation on scroll

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
     <>
       {/* <!-- Hero --> */}
       <div className="hero grid md:grid-cols-2 item-center pt-10 xl:gap-0 gap-6 grid-cols-1">
-        <div>
+        <div className="animate__animated animate__fadeInUp animate__delay-3s">
           <div className="flex items-center gap-3 mb-6 bg-zinc-800 w-fit p-4 rounded-2xl">
             <img
               src={DataImage.HeroImage}
@@ -43,7 +43,7 @@ function App() {
         <img
           src={DataImage.HeroImage}
           alt="Hero Image"
-          className="w-125 md:ml-auto"
+          className="w-125 md:ml-auto animate__animated animate__fadeInUp animate__delay-4s"
           loading="lazy"
         />
       </div>
@@ -51,7 +51,11 @@ function App() {
 
       {/* <!-- About --> */}
       <div className="about mt-32 py-10">
-        <div className="xl:w-2/3 lg:w-3/4 w-full mx-auto rounded-lg p-7 bg-zinc-800">
+        <div
+          className="xl:w-2/3 lg:w-3/4 w-full mx-auto rounded-lg p-7 bg-zinc-800"
+          data-aos="fade-up"
+          data-aos-duration="1000"
+        >
           <img
             src={DataImage.HeroImage}
             alt="Image"
@@ -90,13 +94,29 @@ function App() {
         </div>
 
         <div className="tools mt-32">
-          <h1 className="text-4xl/snug font-bold mb-4">Tools</h1>
-          <p className="text-base/loose xl:w-2/5 lg:w-2/4 md:w-2/3 w-full sm:w-3/4 opacity-50">
+          <h1
+            className="text-4xl/snug font-bold mb-4"
+            data-aos="fade-up"
+            data-aos-duration="1000"
+          >
+            Tools
+          </h1>
+          <p
+            className="text-base/loose xl:w-2/5 lg:w-2/4 md:w-2/3 w-full sm:w-3/4 opacity-50"
+            data-aos="fade-up"
+            data-aos-duration="1000"
+            data-aos-delay="300"
+          >
             These are the tools I usually use for development projects.
           </p>
           <div className="tools-box mt-14 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-4">
             {listTools.map((tool) => (
-              <div className="flex items-center gap-2 p-3 border border-zinc-600 rounded-md hover:bg-zinc-800 group key={tool.id}>">
+              <div
+                className="flex items-center gap-2 p-3 border border-zinc-600 rounded-md hover:bg-zinc-800 group key={tool.id}>"
+                data-aos="fade-up"
+                data-aos-duration="1000"
+                data-aos-delay={tool.dad}
+              >
                 <img
                   src={tool.gambar}
                   alt="Tolls Image"
@@ -116,13 +136,30 @@ function App() {
 
       {/* <!-- Projects --> */}
       <div className="project mt-32 py-10">
-        <h1 className="text-center text-4xl font-bold mb-2">Projects</h1>
-        <p className="text-base/loose opacity-50 text-center">
+        <h1
+          className="text-center text-4xl font-bold mb-2"
+          data-aos="fade-up"
+          data-aos-duration="1000"
+        >
+          Projects
+        </h1>
+        <p
+          className="text-base/loose opacity-50 text-center"
+          data-aos="fade-up"
+          data-aos-duration="1000"
+          data-aos-delay="300"
+        >
           Here are some projects that I have worked on.
         </p>
         <div className="project-box mt-14 grid lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-4">
           {listProyek.map((proyek) => (
-            <div key={proyek.id} className="p-4 bg-zinc-800 rounded-md">
+            <div
+              key={proyek.id}
+              className="p-4 bg-zinc-800 rounded-md"
+              data-aos="fade-up"
+              data-aos-duration="1000"
+              data-aos-delay={proyek.dad}
+            >
               <img src={proyek.gambar} alt="Proyek Image" loading="lazy" />
               <div>
                 <h1 className="font-bold text-2xl my-4">{proyek.nama}</h1>
@@ -154,14 +191,28 @@ function App() {
 
       {/* <!-- Contact --> */}
       <div className="contact mt-32 sm:p-10 p-0">
-        <h1 className="text-4xl mb-2 font-bold text-center">Contact</h1>
-        <p className="text-base/loose opacity-50 text-center mb-10">
+        <h1
+          className="text-4xl mb-2 font-bold text-center"
+          data-aos="fade-up"
+          data-aos-duration="1000"
+        >
+          Contact
+        </h1>
+        <p
+          className="text-base/loose opacity-50 text-center mb-10"
+          data-aos="fade-up"
+          data-aos-duration="1000"
+          data-aos-delay="600"
+        >
           Feel free to reach out for collaborations or just a friendly hello!
         </p>
         <form
           action="https://formsubmit.co/clipnime27@gmail.com"
           method="POST"
           className="bg-zinc-800 p-10 sm:w-fit w-full mx-auto rounded-md"
+          data-aos="fade-up"
+          data-aos-duration="1000"
+          data-aos-delay="600"
           autoComplete="off"
         >
           <div className="flex flex-col gap-6">
