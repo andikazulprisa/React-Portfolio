7c1c6f32c97b5bb4609c166007d464f6d84174cc
Andika Zulprisa Adha <andikazulprisa27@gmail.com>
2025-12-28 20:35:28 +0700

    fix: fix bug main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from "./App.jsx";
 import Navbar from "./components/Navbar.jsx";
 import "remixicon/fonts/remixicon.css";
 import Footer from "./components/Footer.jsx";
-import Preloader from "./components/Preloader.jsx";
+import Preloader from "./components/PreLoader.jsx";
 import "animate.css";
 import AOS from "aos";
 import "aos/dist/aos.css"; // You can also use <link> for styles
